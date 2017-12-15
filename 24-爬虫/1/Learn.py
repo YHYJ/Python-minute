0927c2d3c9bc5856bd0335c7fe0c7f2a9c822c15
YJ1516 <yj1516268@outlook.com>
2017-12-15 13:28:58 +0800

    24

---
+++ 24-爬虫/1/Learn.py
@@ -0,0 +1,20 @@
+import urllib.parse
+import urllib.request
+
+
+data = {}
+# data["word"] = "Jecvay Notes"
+
+url = "https://www.baidu.com/"
+# url_values = urllib.parse.urlencode(data)
+full_url = url
+
+data = urllib.request.urlopen(full_url).read()
+data = data.decode("UTF-8")
+# headers = {"User-Agent": "Baiduspider+"}
+
+
+if __name__ == '__main__':
+    print(type(data), data)  # 爬得的数据是字符串
+    with open("data/Learn.html", "w") as f:
+        f.write(data)
