f09697a8ae971b086bba9e5faad2b92429d26243
YJ <YJ1516268@outlook.com>
2018-09-03 10:18:20 +0800

    2018-09-03上午 10时18分20秒

---
+++ 5-格式化输出和编码问题/1-string的格式化_新式format方法格式化/字符串格式化方法——str和repr.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+
+# repr() ——用来产生一个方便机器解读的字符串形式，输出有特殊作用的字符，比如转译字符\n
+# str() ——用来产生一个方便人解读的字符串形式，输出纯字符串
+"""对于一个没有可供人方便解读的特殊形式，str()返回值与 repr()返回值相同.
+
+对于数字或者类似于列表和字典的结构，两种方法可以产生同样的表现形式
+仅对字符串有两个不同的表现"""
+print('*'*50, '字符串')
+
+s = 'Hello,world'
+print(str(s))   # 输出Hello,world
+print(repr(s))  # 输出'Hello,world'
+# 对于许多对象类型，包括大多数内置的，eval（repr（obj））== obj
+print(eval(repr(s)))  # 输出Hello,world
+
+h = 'hello,world\n'
+print('h = '+h)     # 单纯输出h转义字符起作用
+print('str(h) = '+str(h))   # 使用str函数转义字符起作用
+print('repr(h) = '+repr(h))     # 使用reor函数将整个h当作一字符串
+
+
+print('*'*50, '数字')
+
+print(str(1/7))
+print(repr(1/7))
+x = 10*3.25
+y = 200*200
+s = 'x的值是' + repr(x) + '，y的值是' + repr(y) + '……'
+print(s)
+s = 'x的值是' + str(x) + '，y的值是' + str(y) + '……'
+print(s)
---
+++ 5-格式化输出和编码问题/3-string的格式化_旧式%格式化/string_FAT_Add_Game.py
@@ -0,0 +1,33 @@
+﻿#!/home/yj/.virtualenvs/py3.6.2/bin/python
+# -*- coding: utf-8 -*-
+
+#把bool加入到猜数字小游戏
+#把while加入到猜数字小游戏
+#把随机数模块加入到猜数字小游戏
+#把字符串格式化加入到猜数字小游戏
+# -*- coding: utf-8 -*-
+from random import randint  #===或者这个写法：import random===#
+num = randint(1,100)        #===num = random.randint(1,100)#
+
+guess = False
+print('猜一个数字，范围是0~100！')
+
+while guess == False:
+    answer = int(input('请输入你猜的数字：'))
+
+    if answer < num:
+        print('%d 太小！'%answer)
+        #print(answer)
+
+    elif answer > num:      #elif等效于C的else if，后面加条件
+        print('%d 太大！'%answer)
+        #print(answer)
+
+    else:                  #else后面不能加条件
+        print('恭喜猜对！！！')
+        print('这个数字是 %d'%answer)
+        guess = True
+    if answer < 0:
+        print('即将退出游戏！')
+        break
+#eval(input('回车键退出'))
