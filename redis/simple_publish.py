eb15f6d1ffbe46d0bf98ca8246244eeb386611fd
YJ <YJ1516268@outlook.com>
2018-12-21 09:01:15 +0800

    pub/sub

---
+++ redis/simple_publish.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Author: YJ
+Email: yj1516268@outlook.com
+Created Date: 2018-12-21 01:33:20
+
+Redis pub/sub mode.
+"""
+
+import redis
+import time
+
+
+def main():
+    """A simple redis pub/sub example.
+    A publisher.
+    :returns: TODO
+
+    """
+    redi = redis.Redis()
+    while redi.ping():
+        # rediis.Redis.publish(channel, message)
+        redi.publish('Channel1', '1 Test')
+        time.sleep(1)
+        redi.publish('Channel2', '2 Test')
+        time.sleep(1)
+        redi.publish('Bye', 'exit')
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ redis/simple_subscribe.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Author: YJ
+Email: yj1516268@outlook.com
+Created Date: 2018-12-21 01:44:04
+
+Redis pub/sub mode.
+"""
+
+import redis
+
+
+def main():
+    """A simple redis pub/sub example.
+    A subscriber.
+    :returns: TODO
+
+    """
+    redi = redis.Redis()
+    ps = redi.pubsub()
+    # ps.subscribe(['<Channel>, <Channel>'])
+    # ps.psubscribe(['<Channel Mode>, <Channel Mode>'])
+    ps.psubscribe(['Channel?', 'Bye'])
+    for im in ps.listen():
+        # im = {'type': 'pmessage', 'pattern': b'<Channel Mode>', 'channel': b'<Channel>', 'data': b'<message>'}
+        if im['type'] == 'message':
+            print(
+                'Type <{}> channel <{}> message -- {}.'.format(
+                    im['type'],
+                    im['channel'].decode('utf-8'),
+                    im['data'].decode('utf-8'),
+                )
+            )
+        elif im['type'] == 'pmessage':
+            print(
+                'Type <{}> channel <{}/{}> message -- {}.'.format(
+                    im['type'],
+                    im['pattern'].decode('utf-8'),
+                    im['channel'].decode('utf-8'),
+                    im['data'].decode('utf-8'),
+                )
+            )
+
+
+if __name__ == "__main__":
+    main()
