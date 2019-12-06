afa564b1759e7e2be05b7856251e227d26b0e270
YJ <YJ1516268@outlook.com>
2019-12-06 10:53:42 +0800

    更新UDP

--- 20-网络编程/5-UDP编程/3-client.py
+++ 20-网络编程/5-UDP编程/3-client.py
@@ -9,6 +9,8 @@ import socket
 # 首先创建基于UDP的Socket
 '''不需要调用connect()，直接通过sendto()给服务器发数据'''
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.settimeout(5)
+#  s.connect(('127.0.0.1', 9986))
 datas = []
 
 while True:
@@ -18,6 +20,7 @@ while True:
     else:
         break
 for data in datas:
-    s.sendto(data, ('127.0.0.1', 9986))  # 发送数据：因为server绑定的是0.0.0.0，所以发送到127.0.0.1也可以
+    s.sendto(data, ('127.0.0.1', 9986))  # 不需要connect，直接发送到指定地址
+    #  s.send(data)                      # send和connect配合使用，当没有服务器bind到指定端口时立即报错
     print(s.recv(1024).decode('utf-8'))  # 接收数据
 s.close()
