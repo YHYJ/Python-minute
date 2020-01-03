60338cd2e26bc9ac70d35db73db1ac046ba2de63
YJ <YJ1516268@outlook.com>
2020-01-03 16:08:54 +0800

    TCP更新

--- 20-网络编程/4-TCP编程/client_2.py
+++ 20-网络编程/4-TCP编程/client_2.py
@@ -5,18 +5,60 @@
 
 
 import socket
+import time
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('', 6799))
+from threading import Thread
 
-# print(client_socket.recv(1024).decode('utf-8'))
 
-while True:
-    data = input('Please input: ')
-    if data == 'q' or data == 'Q':
-        break
-    else:
-        client_socket.send(data.encode('utf-8'))
-        print('The server returned : %s\n' % client_socket.recv(1024).decode('utf-8'))
-client_socket.send('q'.encode('utf-8'))
-client_socket.close()
+def doConnect(host, port):
+    """TODO: Docstring for doConnect.
+    :returns: TODO
+
+    """
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.connect((host, port))
+    except Exception:
+        pass    # 关键：尝试建立连接不可行的话跳过报错再次尝试
+
+    return sock
+
+
+def main():
+    """TODO: Docstring for main.
+    :returns: TODO
+
+    """
+    host = '127.0.0.1'
+    port = 6799
+
+    time_tag = str(time.time())
+    data = '{} - {}'.format('My Test', time_tag)
+
+    sock = doConnect(host, port)
+
+    for i in range(3):
+        time.sleep(2)
+        while True:
+            try:
+                sock.sendall(data.encode('utf-8'))
+                sdata = sock.recv(1024).decode('utf-8')
+                print('Server returned: {}'.format(sdata))
+                #  time.sleep(2)
+                break
+            except socket.error:    # 如果报socket连接错误则尝试释放资源重新连接
+                print('正在重建socket连接...')
+                sock.close()
+                time.sleep(2)
+                sock = doConnect(host, port)
+            except Exception as err:
+                raise err
+
+
+if __name__ == "__main__":
+    #  main()
+    # # or # #
+    thread = Thread(target=main, name='main')
+    thread.setDaemon(True)
+    thread.start()
+    thread.join()
--- 20-网络编程/4-TCP编程/servers_2.py
+++ 20-网络编程/4-TCP编程/servers_2.py
@@ -8,25 +8,26 @@ import socket
 import threading
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)	 # 立即收回端口
+# 立即收回端口
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
 server_socket.bind(('', 6799))
 server_socket.listen(5)
 
-print('[info]:Waiting for connection...')
+print('[info]:Waiting for connection...\n')
 
 
 def tcplink(cs_sock, client_addr):
-    print('[info]:%s:%s connected' % client_addr)
-    cs_sock.send('Welcome!'.encode('utf-8'))
+    print('\n[info]:%s:%s connected' % client_addr)
     while True:
         data = cs_sock.recv(1024)
-        if not data or data.decode('utf-8') == 'q' or data.decode('utf-8') == 'Q':
+        if not data or data.decode('utf-8') in ['Q', 'q']:
             cs_sock.close()
             # server_socket.close()
             print('[info]:Connection %s:%s is closed' % client_addr)
             break
         print('Client information: %s' % data.decode('utf-8'))
         cs_sock.send(data.decode('utf-8').encode('utf-8'))
+
 
 while True:
     cs_socket, client_addr = server_socket.accept()
